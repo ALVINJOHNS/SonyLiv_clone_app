6e2c2fd6e57f5387e8be057d4c3b617c8f1ef449
ALVINJOHNS <alvinjohnson2016@gmail.com>
2024-06-06 18:48:42 +0530

    search screen updated

--- lib/presentation/search/search_screen.dart
+++ lib/presentation/search/search_screen.dart
@@ -1,6 +1,4 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class SearchScreen extends StatefulWidget {
